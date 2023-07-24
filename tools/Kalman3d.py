bc0b5e07a992f57d30dc751ac084e91411ac58d7
Ben Oxley <ben.oxley@gmail.com>
2023-07-24 21:06:10 +0100

    Working filter

--- tools/Kalman3d.py
+++ tools/Kalman3d.py
@@ -29,23 +29,27 @@ class Kalman3d:
         eastings = np.array(eastings)
         northings = np.array(northings)
         self.alt_relative = (
-            (altitudes - altitudes[0]) * -1.0
-        )  # convert to relative, down is positive
-        
+            altitudes - altitudes[0]
+        ) * -1.0  # convert to relative, down is positive
+
         self.eastings = eastings - eastings[0]  # convert to relative
         self.northings = northings - northings[0]  # convert to relative
-    
 
     """Interpolate the GPS to the same timescales as the imu data"""
 
     def interpolate_gps(self):
-        self.eastings = interpolate.interp1d(self.gpstime, self.eastings, fill_value="extrapolate")(self.imutime)
-        self.northings = interpolate.interp1d(self.gpstime, self.northings, fill_value="extrapolate")(
-            self.imutime
-        )
-        self.alt_relative = interpolate.interp1d(self.gpstime, self.alt_relative, fill_value="extrapolate")(
-            self.imutime
-        )
+        self.eastings = interpolate.interp1d(
+            self.gpstime, self.eastings, kind="previous", fill_value="extrapolate"
+        )(self.imutime)
+        self.northings = interpolate.interp1d(
+            self.gpstime, self.northings, kind="previous", fill_value="extrapolate"
+        )(self.imutime)
+        self.alt_relative = interpolate.interp1d(
+            self.gpstime, self.alt_relative, fill_value="extrapolate"
+        )(self.imutime)
+        self.eastings = np.nan_to_num(self.eastings, nan=0)
+        self.northings = np.nan_to_num(self.northings, nan=0)
+        self.alt_relative = np.nan_to_num(self.alt_relative, nan=0)
 
     def process(self):
         self.llm2utm()
@@ -68,12 +72,14 @@ class Kalman3d:
         # Measurement noise
         gps_var = 1e-6
         imu_var = 1e-3
+        #Error definitions
         R = np.diag([gps_var, gps_var, gps_var * 10, imu_var, imu_var, imu_var])
 
         # Discrete time model
         # s_n(k+1) = s_n(k) + v_n(k) * dt
         # v_n(k+1) = v_n(k) + a_n(k) * dt
         # a_n(k+1) = a_n(k)
+        #Relationship between states
         F = np.array(
             [
                 [1, 0, 0, dt, 0, 0, 0, 0, 0],
@@ -88,6 +94,7 @@ class Kalman3d:
             ]
         )
 
+        #Sensor to state mapping
         H = np.array(
             [
                 [1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -105,7 +112,16 @@ class Kalman3d:
         self.an = self.an * 9.81
         self.ae = self.ae * 9.81
         self.ad = self.ad * 9.81
-        sensors = np.stack((self.northings, self.eastings, self.alt_relative, self.an, self.ae, self.ad))
+        sensors = np.stack(
+            (
+                self.northings,
+                self.eastings,
+                self.alt_relative,
+                self.an,
+                self.ae,
+                self.ad,
+            )
+        )
         kal_x_stor = np.zeros((9, n_steps))
 
         for i in range(0, n_steps - 1):
@@ -113,14 +129,16 @@ class Kalman3d:
             xp = F @ x  # no inputs
             Pp = F @ P @ F.T + Q
             # Update
-            sensor_step = np.array([sensors[:,i]]).T # Not sure about this. Ask Jon
+            sensor_step = np.array(
+                [sensors[:, i]]
+            ).T  # Re-arranging the array to get the element dimentsions right and conver to 2d array
             y = sensor_step - H @ x
             S = H @ P @ H.T + R
             K = Pp @ H.T @ np.linalg.inv(S)
             x = xp + K @ y
             P = (np.identity(9) - K @ H) @ Pp
             # Store
-            kal_x_stor[:, i] = x[:,0]
+            kal_x_stor[:, i] = x[:, 0]
         self.kal_x_store = kal_x_stor
 
     def result(self):
